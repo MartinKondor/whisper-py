5d8c217be3b5bdcf4c6e153f88a75609d015b644
Martin Kondor <martinkondor@gmail.com>
2022-12-01 10:39:18 +0100

    details

--- pybind.sh
+++ pybind.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#
-# WARNING
-# ONLY USE THIS FILE IN THE ROOT FOLDER OF WHISPER.CPP
-#
+########################################################
+#                                                      #
+####################!!WARNING!!#########################
+# ONLY USE THIS FILE IN THE ROOT FOLDER OF WHISPER.CPP #
+#                                                      #
+########################################################
 make
 gcc -O3 -std=c11   -pthread -mavx -mavx2 -mfma -mf16c -fPIC -c ggml.c
 g++ -O3 -std=c++11 -pthread --shared -fPIC -static-libstdc++ whisper.cpp ggml.o -o libwhisper.so
-# python app.py
