d88b9be705070ba6c53c369548713ade79fe60c4
Martin Kondor <martinkondor@gmail.com>
2022-12-16 14:37:50 +0100

    Update

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 
 Python bindings for [whisper.cpp](https://github.com/ggerganov/whisper.cpp)
 
-_Latest working commit on the main branch: **4698dcd**_
+_Latest working commit on the main branch: **5a5c5dd**_
 
 ## How to setup
 
 1. Clone this repository.
-2. Clone [whisper.cpp](https://github.com/ggerganov/whisper.cpp).
+2. Clone [MartinKondor/whisper.cpp](https://github.com/MartinKondor/whisper.cpp/tree/python-binding).
 3. Copy `pybind.sh` and `whisper.py` from this repository to the root folder of `whisper.cpp`.
-4. Download a base language model according to [whisper.cpp](https://github.com/ggerganov/whisper.cpp)'s `README`
+4. Download a base language model according to [MartinKondor/whisper.cpp](https://github.com/MartinKondor/whisper.cpp/tree/python-binding)'s `README`
 5. Run: `sh pybind.sh`.
 6. Test if everything works with: `python whisper.py`.
 7. You can build on `whisper.py` further.
--- whisper.py
+++ whisper.py
@@ -8,7 +8,8 @@ whisper_folder = "."
 libname     = f"{whisper_folder}/libwhisper.so"
 fname_model = f"{whisper_folder}/models/ggml-base.en.bin"
 fname_wav   = f"{whisper_folder}/samples/jfk.wav"
-is_verbose = True
+language = b"en"
+is_verbose = False
 
 # this needs to match the C struct in ./whisper.h
 class WhisperFullParams(ctypes.Structure):
@@ -45,6 +46,39 @@ class WhisperFullParams(ctypes.Structure):
     ]
 
 
+def whisper_full_default_params(def_params: WhisperFullParams) -> WhisperFullParams:
+    params = WhisperFullParams()
+    params.strategy = 0
+    params.n_threads = 1
+    params.n_max_text_ctx = 16384
+    params.offset_ms = 0
+    params.duration_ms = 0
+    params.translate = 0
+    params.no_context = 0
+    params.single_segment = 0
+    params.print_special = 0
+    params.print_progress = 1
+    params.print_realtime = 0
+    params.print_timestamps = 1
+    params.token_timestamps = 0
+    params.thold_pt = 0.01
+    params.thold_ptsum = 0.01
+    params.max_len = 0
+    params.max_tokens = 0
+    params.speed_up = 0
+    params.audio_ctx = 0
+    params.prompt_tokens = def_params.prompt_tokens
+    params.prompt_n_tokens = 0
+    params.language = def_params.language
+    params.greedy = def_params.greedy
+    params.beam_search = def_params.beam_search
+    params.new_segment_callback = None
+    params.new_segment_callback_user_data = None
+    params.encoder_begin_callback = None
+    params.encoder_begin_callback_user_data = None
+    return params
+
+
 def format_time(t: int) -> str:
     millis = int(t*10)
     seconds = (millis/1000)%60
@@ -65,6 +99,7 @@ if __name__ == "__main__":
         print("tell Python what are the return types of the functions")
     whisper.whisper_init.restype                  = ctypes.c_void_p
     whisper.whisper_full_default_params.restype   = WhisperFullParams
+    whisper._whisper_full_default_params.restype   = WhisperFullParams
     whisper.whisper_full_get_segment_text.restype = ctypes.c_char_p
 
     if is_verbose:
@@ -73,7 +108,14 @@ if __name__ == "__main__":
 
     if is_verbose:
         print("get default whisper parameters and adjust as needed")
-    params = whisper.whisper_full_default_params(0)
+    _params = whisper._whisper_full_default_params(0, language)
+    params = whisper_full_default_params(_params)
+
+    # set parameters
+    params.print_special = is_verbose
+    params.print_progress = is_verbose
+    params.print_realtime = is_verbose
+    params.print_timestamps = is_verbose
 
     if is_verbose:
         print("load WAV file")
@@ -82,11 +124,6 @@ if __name__ == "__main__":
     if is_verbose:
         print("convert to 32-bit float")
     data = data.astype('float32')/32768.0
-
-    params.print_special = is_verbose
-    params.print_progress = is_verbose
-    params.print_realtime = is_verbose
-    params.print_timestamps = is_verbose
 
     if is_verbose:
         print("run the inference")
@@ -116,3 +153,4 @@ if __name__ == "__main__":
         print("free the memory")
         
     whisper.whisper_free(ctypes.c_void_p(ctx))
+    
